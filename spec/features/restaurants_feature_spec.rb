13026b932a8efe7bce7d7fea0796285dc45f4b08
Alex Peattie <alexpeattie@gmail.com>
2014-08-12 16:00:04 +0100

    Add validations

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -21,19 +21,6 @@ describe 'restaurants' do
   end
 
   describe 'creating restaurants' do
-    it 'prompts the user to fill out a form, then displays the new restaurant' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-
-      expect(page).to have_content 'KFC'
-      expect(current_path).to eq '/restaurants'
-    end
-  end
-
-  describe 'creating restaurants' do
     before(:each) do
       Restaurant.create(name: 'KFC')
     end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is valid with a unique name of > 2 characters, starting with a capital letter' do
+    restaurant = Restaurant.new(name: 'Nandos')
+    expect(restaurant).to have(:no).errors_on(:name)
+  end
+  
+  it 'is not valid with a name of < 2 characters' do
+    restaurant = Restaurant.new(name: 'Ab')
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
+  it 'is not valid if it does not start with an uppercase letter' do
+    restaurant = Restaurant.new(name: 'nandos')
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
+  it 'is not valid if it is not unique' do
+    Restaurant.create(name: 'Nandos')
+    restaurant = Restaurant.new(name: 'Nandos')
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
 end
