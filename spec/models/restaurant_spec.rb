6260964a8be1a46aabc7eb4bd3014f955bf862aa
Alex Peattie <alexpeattie@gmail.com>
2014-08-13 11:08:15 +0100

    Add reviews w/ stars

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,25 +2,61 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
 
-  it 'is valid with a unique name of > 2 characters, starting with a capital letter' do
-    restaurant = Restaurant.new(name: 'Nandos')
-    expect(restaurant).to have(:no).errors_on(:name)
-  end
-  
-  it 'is not valid with a name of < 2 characters' do
-    restaurant = Restaurant.new(name: 'Ab')
-    expect(restaurant).to have(1).error_on(:name)
+  let(:restaurant) { Restaurant.create(name: 'Nandos') }
+
+  describe '#average_rating' do
+    context 'no reviews' do
+      it 'returns N/A' do
+        expect(restaurant.average_rating).to eq 'N/A'
+      end
+    end
+
+    context '1 review' do
+      it 'returns that rating' do
+        restaurant.reviews.create(rating: 4)
+        expect(restaurant.average_rating).to eq 4
+      end
+    end
+
+    context 'multiple reviews' do
+      it 'return the average of all ratings' do
+        restaurant.reviews.create(rating: 3)
+        restaurant.reviews.create(rating: 5)
+
+        expect(restaurant.average_rating).to eq 4
+      end
+
+      it 'returns a float, if average is a non-whole number' do
+        restaurant.reviews.create(rating: 4)
+        restaurant.reviews.create(rating: 5)
+
+        expect(restaurant.average_rating).to eq 4.5
+      end
+    end
+
   end
 
-  it 'is not valid if it does not start with an uppercase letter' do
-    restaurant = Restaurant.new(name: 'nandos')
-    expect(restaurant).to have(1).error_on(:name)
-  end
+  describe 'validations' do
+    it 'is valid with a unique name of > 2 characters, starting with a capital letter' do
+      restaurant = Restaurant.new(name: 'Nandos')
+      expect(restaurant).to have(:no).errors_on(:name)
+    end
+    
+    it 'is not valid with a name of < 2 characters' do
+      restaurant = Restaurant.new(name: 'Ab')
+      expect(restaurant).to have(1).error_on(:name)
+    end
 
-  it 'is not valid if it is not unique' do
-    Restaurant.create(name: 'Nandos')
-    restaurant = Restaurant.new(name: 'Nandos')
-    expect(restaurant).to have(1).error_on(:name)
+    it 'is not valid if it does not start with an uppercase letter' do
+      restaurant = Restaurant.new(name: 'nandos')
+      expect(restaurant).to have(1).error_on(:name)
+    end
+
+    it 'is not valid if it is not unique' do
+      Restaurant.create(name: 'Nandos')
+      restaurant = Restaurant.new(name: 'Nandos')
+      expect(restaurant).to have(1).error_on(:name)
+    end
   end
 
 end
